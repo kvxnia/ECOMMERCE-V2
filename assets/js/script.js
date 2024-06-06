12116793833d7d545a0a4f1ab54aa635988c1ca2
Kexaty_ <eliasbenjaminserrudo@gmail.com>
2024-06-06 19:33:24 -0300

    m

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@ function verifi() {
     var password = document.getElementById("password").value;
 
     if (username == "elias" && password == "123") {
+        
         alert("¡Inicio de sesión correcto!");
+        
     } else {
         alert("Usuario o contraseña incorrectos");
     }
